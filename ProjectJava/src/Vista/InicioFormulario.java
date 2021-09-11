37aac0e04704055d516c67c96a6aca7a20ae2cbb
Chris <Chris@DESKTOP-HN871M9>
2021-09-10 21:53:06 -0500

    Iniciando formulario de la app desde la pc principal.

---
+++ ProjectJava/src/Vista/InicioFormulario.java
@@ -0,0 +1,13 @@
+package Vista;
+
+import javax.swing.JFrame;
+
+public class InicioFormulario {
+	private JFrame ventanaInicio;
+	
+	public InicioFormulario() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
